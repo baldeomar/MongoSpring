94baf128e4e32e139065cfd31687ff819a2d36cc
Oumar BALDE <baldeomar@yahoo.fr>
2015-10-11 19:20:46 +0200

    Appli urls

---
+++ src/main/java/com/oumar/learn/Application/ApplicationUrl.java
@@ -0,0 +1,12 @@
+package com.oumar.learn.Application;
+
+/**
+ * Created by oumar on 11/10/15.
+ */
+public final class ApplicationUrl {
+    public static final String ROOT = "/";
+    public static final String REGISTER = "/register";
+    public static final String ADMIN = "/admin**";
+    public static final String LOGIN = "/login";
+    public static final String WELCOME = "/welcome**";
+}
